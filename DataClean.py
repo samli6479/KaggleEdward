37785b83aae4d0ed651bd9b389c0c46a47fcb113
samli6479 <samli6479@users.noreply.github.com>
2016-07-28 14:46:28 -0400

    Update DataClean.py

--- DataClean.py
+++ DataClean.py
@@ -1,3 +1,30 @@
 import numpy as np
 import pandas as pd
 import csv
+import copy
+
+def filereader(filename,filetype='csv',header=True):
+    '''read the raw data and return numpy array'''
+    
+    opener = open(filename)
+    
+    if filetype == 'csv':
+      reader = csv.read(opener,delimiter=',')
+      
+      if header:
+         head = next(reader)
+         
+      data = [tuple(row) for row in reader]
+    
+    return np.array(data)
+    
+    if filetype == 'txt':
+      data = [line.rstrip('\r\n') for line in opener]
+      data = list(map(lambda x: x.split(' '), data))
+      
+      if header:
+         head = data[0]
+         
+         data = list(filter(lambda a: a != head,data))
+      
+    return np.array(data)
